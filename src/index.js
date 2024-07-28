89f11bdecf3637527dc446e84ba885a74caf3207
hoolookook <hoolookook@gmail.com>
2024-07-28 20:29:03 +0900

    react dicegame first

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>안녕 리액트!</h1>,
+  document.getElementById('root')
+);
